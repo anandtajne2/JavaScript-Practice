c20a2a58b775f91e2004608e2896980d87be16ba
anand-tajne <anand.tajne09@gmail.com>
2026-01-22 12:29:13 +0530

    conversion of datatypes

---
+++ 01_basics/03_cnoversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "33"   // 33 
+// let score = null    // 0
+// let score = undefined  // NaN
+// let score = true
+let score = "Anand"  // NaN
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn = 1;  //  1 - true 0 - false
+let isLoggedIn = "";  //  "" - false | "anand" - true
+
+let valueInBoolean = Boolean(isLoggedIn)
+// console.log(valueInBoolean)
+
+let someNumber = 33  // 
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
